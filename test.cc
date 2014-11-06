7fae49ef45b314f84366e2af46d8e46f131b9f5e
Brett Hall <bretthall@fastmail.fm>
2014-11-05 21:12:01 -0800

    more testing

--- test.cc
+++ test.cc
@@ -12,8 +12,24 @@ struct S2
    int value;
 };
 
+struct S2Derived : public S2
+{
+   S2Derived (const int x) :
+      S2 {x}
+   {}
+};
+
 struct S3
-{};
+{
+   int value;
+};
+
+struct S3Derived : public S3
+{
+   S3Derived (const int x) :
+      S3 {x}
+   {}
+};
 
 struct S4
 {};
@@ -27,6 +43,8 @@ void Print (const boost::any& arg)
       arg,
       Case<S1>([](const S1& a) {std::cout << "S1(" << a.value << ")"<< std::endl;}),
       Case<S2>([](const S2& a) {std::cout << "S2(" << a.value << ")"<< std::endl;}),
+      Case<S2Derived>([](const S2Derived& a) {std::cout << "S2D(" << a.value << ")"<< std::endl;}),
+      //Case<S3Derived>([](const S3Derived& a) {std::cout << "S3D(" << a.value << ")"<< std::endl;}),
       OneOf<S3, S4>(
          [](const boost::any& a)
          {
@@ -43,14 +61,27 @@ void Print (const boost::any& arg)
 
 int main ()
 {
+   const auto s2d = boost::any (S2Derived {974496});
+   if (boost::any_cast<S2>(&s2d))
+   {
+      std::cout << "Got base ptr" << std::endl;
+   }
+   //boost::any_cast does not take inheritance into account!!!!
+   
    std::cout << "S1 = ";
    Print (S1 {299298});
 
    std::cout << "S2 = ";
    Print (S2 {984707});
 
+   std::cout << "S2Derived = ";
+   Print (S2Derived {779639});
+
    std::cout << "S3 = ";
    Print (S3 ());
+
+   std::cout << "S3Derived = ";
+   Print (S3Derived {376581});
 
    std::cout << "S4 = ";
    Print (S4 ());
