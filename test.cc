11e0ebc79175c985060de2086bc947afe4614a9b
Brett Hall <bretthall@fastmail.fm>
2015-03-24 22:01:51 -0700

    Added documentation

--- test.cc
+++ test.cc
@@ -3,10 +3,17 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+//Example program for the TypeSwitch libraray.
+//Compile with
+//   clang++ --std=c++1y test.cc  -o test
+//or
+//   g++ --std=c++1y test.cc  -o test
+
 #include "TypeSwitch.h"
 
 #include <iostream>
 
+//Vairous test classes.
 struct S1
 {
    int value;
@@ -15,6 +22,12 @@ struct S1
 struct S2
 {
    int value;
+
+   S2 (const int x):
+      value {x}
+   {}
+   
+   virtual ~S2 () {}
 };
 
 struct S2Derived : public S2
@@ -26,15 +39,11 @@ struct S2Derived : public S2
 
 struct S3
 {
-   int value;
+   virtual ~S3 () {}
 };
 
 struct S3Derived : public S3
-{
-   S3Derived (const int x) :
-      S3 {x}
-   {}
-};
+{};
 
 struct S4
 {};
@@ -42,6 +51,7 @@ struct S4
 struct S5
 {};
 
+//Uses TypeSwitch to print different messages based on the type stored in the argument.
 void PrintAny (const boost::any& arg)
 {
    using namespace TypeSwitch;
@@ -58,7 +68,8 @@ void PrintAny (const boost::any& arg)
          {
             std::cout << "S2(" << a.value << ")"<< std::endl;
          }),
-      Case<S2Derived>(
+      Case<S2Derived>( //Note that boost::any_cast ignores inheritance so a S2Derived will not be
+                       //handled by the Case<S2> above in this case
          [](const S2Derived& a)
          {
             std::cout << "S2D(" << a.value << ")"<< std::endl;
@@ -82,6 +93,7 @@ void PrintAny (const boost::any& arg)
       );
 }
 
+//Used TypeSwitch to print different messages based on the type of arg. See notes below
 void PrintS2 (const S2& arg)
 {
    using namespace TypeSwitch;
@@ -91,12 +103,13 @@ void PrintS2 (const S2& arg)
       Case<S2>(
          [](const S2& a)
          {
-            std::cout << "S2(" << a.value << ")"<< std::endl;
+            std::cout << "S2(" << a.value << ")" << std::endl;
          }),
-      Case<S2Derived>(
+      Case<S2Derived>( //An S2Derived will be handled by the S2 case above since we are using
+                       //references this time
          [](const S2Derived& a)
          {
-            std::cout << "S2D(" << a.value << ")"<< std::endl;
+            std::cout << "S2D(" << a.value << ") -- should never gt here !! " << std::endl;
          }),
       Otherwise (
          [](const S2& a)
@@ -106,6 +119,33 @@ void PrintS2 (const S2& arg)
       );
 }
 
+//Used TypeSwitch to print different messages based on the type of arg. This one handles inheritance properly. 
+void PrintS2Corrected (const S2& arg)
+{
+   using namespace TypeSwitch;
+
+   Switch (
+      arg,
+      Case<S2Derived>( //We put the S2Derived case first so that it can handle any S2Derived objects
+                       //before the S2 case gets a hold of them
+         [](const S2Derived& a)
+         {
+            std::cout << "S2D(" << a.value << ")" << std::endl;
+         }),
+      Case<S2>(
+         [](const S2& a)
+         {
+            std::cout << "S2(" << a.value << ")" << std::endl;
+         }),
+      Otherwise (
+         [](const S2& a)
+         {
+            std::cout << "Shouldn't get here!" << std::endl;
+         })
+      );
+}
+
+//Prints diferent messages based ont eh contents of a boost::variant
 void PrintVariant (const boost::variant<S1, S2, S3>& v)
 {
    using namespace TypeSwitch;
@@ -125,10 +165,11 @@ void PrintVariant (const boost::variant<S1, S2, S3>& v)
       Case<S3>(
          [](const S3& a)
          {
-            std::cout << "S3(" << a.value << ")"<< std::endl;
+            std::cout << "S3()"<< std::endl;
          }),
       Otherwise (
-         [](const auto& a)
+         [](const auto& a) //c++14 lets unsigned short get away with not having to specify the
+                           //variant type again
          {
             std::cout << "Shouldn't get here" << std::endl;
          })
@@ -140,9 +181,9 @@ int main ()
    const auto s2d = boost::any {S2Derived {974496}};
    if (boost::any_cast<S2>(&s2d))
    {
+      //we'll never get here because boost::any_cast does not take inheritance into account!!!!
       std::cout << "Got base ptr" << std::endl;
    }
-   //boost::any_cast does not take inheritance into account!!!!
    
    std::cout << "S1 = ";
    PrintAny (S1 {299298});
@@ -157,7 +198,7 @@ int main ()
    PrintAny (S3 {});
 
    std::cout << "S3Derived = ";
-   PrintAny (S3Derived {376581});
+   PrintAny (S3Derived {});
 
    std::cout << "S4 = ";
    PrintAny (S4 {});
@@ -171,6 +212,12 @@ int main ()
    std::cout << "S2: S2Derived = ";
    PrintS2 (S2Derived {238263});
 
+   std::cout << "S2 corrected: S2 = ";
+   PrintS2Corrected (S2 {238263});
+   
+   std::cout << "S2 corrected: S2Derived = ";
+   PrintS2Corrected (S2Derived {238263});
+
    std::cout << "Variant: S1 = ";
    PrintVariant (S1 {238263});
 
@@ -178,7 +225,7 @@ int main ()
    PrintVariant (S2 {238263});
 
    std::cout << "Variant: S3 = ";
-   PrintVariant (S3 {168066});
+   PrintVariant (S3 {});
    
    std::cout << "Variant: S2Derived = ";
    PrintVariant (S2Derived {238263});
