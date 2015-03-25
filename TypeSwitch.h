bbd801de2b31ca33f9695cf96c3c93fc3900c876
Brett Hall <bretthall@fastmail.fm>
2015-03-24 19:46:26 -0700

    Added boost::variant cast and tests for reference and variant

--- TypeSwitch.h
+++ TypeSwitch.h
@@ -1,148 +1,160 @@
 #pragma once
 
 #include <boost/any.hpp>
+#include <boost/variant.hpp>
 
-template <typename Src_t, typename Dest_t>
-const Dest_t* SwitchCast (const Src_t& src)
+namespace TypeSwitch
 {
-   return dynamic_cast<const Dest_t*>(&src);
-}
 
-template <typename Dest_t>
-const Dest_t* SwitchCast (const boost::any& src)
-{
-   return boost::any_cast<Dest_t>(&src);
-}
-
-template <typename Arg_t, typename Handler_t>
-struct CaseHandler
-{
-   const Handler_t& m_handler;
-
-   CaseHandler (const Handler_t& h):
-      m_handler (h)
-   {}
-
-   template <typename BaseArg_t>
-   bool operator()(const BaseArg_t& arg) const
+   template <typename Src_t, typename Dest_t>
+   const Dest_t* SwitchCast (const Src_t& src)
    {
-      if (const auto arg_p = SwitchCast<Arg_t>(arg))
+      return dynamic_cast<const Dest_t*>(&src);
+   }
+
+   template <typename Dest_t>
+   const Dest_t* SwitchCast (const boost::any& src)
+   {
+      return boost::any_cast<Dest_t>(&src);
+   }
+
+   template <typename Dest_t, typename ... Types_t>
+   const Dest_t* SwitchCast (const boost::variant<Types_t...>& src)
+   {
+      return boost::get<Dest_t>(&src);
+   }
+
+   template <typename Arg_t, typename Handler_t>
+   struct CaseHandler
+   {
+      const Handler_t& m_handler;
+
+      CaseHandler (const Handler_t& h):
+         m_handler (h)
+      {}
+
+      template <typename BaseArg_t>
+      bool operator()(const BaseArg_t& arg) const
       {
-         m_handler (*arg_p);
+         if (const auto arg_p = SwitchCast<Arg_t>(arg))
+         {
+            m_handler (*arg_p);
+            return true;
+         }
+         else
+         {
+            return false;
+         }
+      }
+   };
+
+   template <typename Arg_t, typename Handler_t>
+   CaseHandler<Arg_t, Handler_t> Case (const Handler_t& handler)
+   {
+      return CaseHandler<Arg_t, Handler_t>(handler);
+   }
+
+   template <typename Base_t>
+   bool IsOneOf (const Base_t& base)
+   {
+      return false;
+   }
+
+   template <typename Arg_t, typename... Args_t, typename Base_t>
+   bool IsOneOf (const Base_t& base)
+   {
+      if (SwitchCast<Arg_t>(base))
+      {
          return true;
       }
       else
       {
-         return false;
+         return IsOneOf<Args_t...> (base);
       }
    }
-};
 
-template <typename Arg_t, typename Handler_t>
-CaseHandler<Arg_t, Handler_t> Case (const Handler_t& handler)
-{
-   return CaseHandler<Arg_t, Handler_t>(handler);
-}
-
-template <typename Base_t>
-bool IsOneOf (const Base_t& base)
-{
-   return false;
-}
-
-template <typename Arg_t, typename... Args_t, typename Base_t>
-bool IsOneOf (const Base_t& base)
-{
-   if (SwitchCast<Arg_t>(base))
+   template <typename Handler_t, typename ... Args_t>
+   struct OneOfHandler
    {
-      return true;
+      const Handler_t& m_handler;
+
+      OneOfHandler (const Handler_t& h):
+         m_handler (h)
+      {}
+
+      template <typename BaseArg_t>
+      bool operator()(const BaseArg_t& arg) const
+      {
+         if (IsOneOf<Args_t...>(arg))
+         {
+            m_handler (arg);
+            return true;
+         }
+         else
+         {
+            return false;
+         }
+      }
+   };
+
+   template <typename ... Args_t, typename Handler_t>
+   OneOfHandler<Handler_t, Args_t...> OneOf (const Handler_t& handler)
+   {
+      return OneOfHandler<Handler_t, Args_t...>(handler);
    }
-   else
+
+   template <typename Handler_t>
+   struct OtherwiseHandler
    {
-      return IsOneOf<Args_t...> (base);
-   }
-}
+      const Handler_t& m_handler;
 
-template <typename Handler_t, typename ... Args_t>
-struct OneOfHandler
-{
-   const Handler_t& m_handler;
-
-   OneOfHandler (const Handler_t& h):
-      m_handler (h)
-   {}
-
-   template <typename BaseArg_t>
-   bool operator()(const BaseArg_t& arg) const
-   {
-      if (IsOneOf<Args_t...>(arg))
+      OtherwiseHandler (const Handler_t& h):
+         m_handler (h)
+      {}
+         
+      template <typename Arg_t>
+      bool operator()(const Arg_t& arg) const
       {
          m_handler (arg);
          return true;
-      }
-      else
+      }         
+   };
+
+   template <typename Handler_t>
+   OtherwiseHandler<Handler_t> Otherwise (const Handler_t& handler)
+   {
+      return OtherwiseHandler<Handler_t>(handler);
+   }
+
+   struct IgnoreOthers
+   {
+      template <typename Arg_t>
+      bool operator()(const Arg_t&) const
       {
-         return false;
+         return true;
+      }         
+   };
+
+   template <typename Arg_t, typename Handler_t>
+   void Switch (const Arg_t& arg, const OtherwiseHandler<Handler_t>& handler)
+   {
+      handler (arg);
+   }
+
+   template <typename Arg_t>
+   void Switch (const Arg_t& arg, const IgnoreOthers&)
+   {}
+
+   template <typename Arg_t, typename Handler_t, typename... Handlers_t>
+   void Switch (const Arg_t& arg, const Handler_t& handler, const Handlers_t&... handlers)
+   {
+      //NOTE: If you get an error about "no specialization for Switch (arg, handler) then your
+      //Switch call is missing either Otherwise or IgnoreOthers at the end of the handlers.
+         
+      if (!handler (arg))
+      {
+         Switch (arg, handlers...);
       }
    }
-};
 
-template <typename ... Args_t, typename Handler_t>
-OneOfHandler<Handler_t, Args_t...> OneOf (const Handler_t& handler)
-{
-   return OneOfHandler<Handler_t, Args_t...>(handler);
-}
-
-template <typename Handler_t>
-struct OtherwiseHandler
-{
-   const Handler_t& m_handler;
-
-   OtherwiseHandler (const Handler_t& h):
-      m_handler (h)
-   {}
-         
-   template <typename Arg_t>
-   bool operator()(const Arg_t& arg) const
-   {
-      m_handler (arg);
-      return true;
-   }         
-};
-
-template <typename Handler_t>
-OtherwiseHandler<Handler_t> Otherwise (const Handler_t& handler)
-{
-   return OtherwiseHandler<Handler_t>(handler);
-}
-
-struct IgnoreOthers
-{
-   template <typename Arg_t>
-   bool operator()(const Arg_t&) const
-   {
-      return true;
-   }         
-};
-
-template <typename Arg_t, typename Handler_t>
-void Switch (const Arg_t& arg, const OtherwiseHandler<Handler_t>& handler)
-{
-   handler (arg);
-}
-
-template <typename Arg_t>
-void Switch (const Arg_t& arg, const IgnoreOthers&)
-{}
-
-template <typename Arg_t, typename Handler_t, typename... Handlers_t>
-void Switch (const Arg_t& arg, const Handler_t& handler, const Handlers_t&... handlers)
-{
-   //NOTE: If you get an error about "no specialization for Switch (arg, handler) then your
-   //Switch call is missing either Otherwise or IgnoreOthers at the end of the handlers.
-         
-   if (!handler (arg))
-   {
-      Switch (arg, handlers...);
-   }
 }
--- test.cc
+++ test.cc
@@ -37,14 +37,27 @@ struct S4
 struct S5
 {};
 
-void Print (const boost::any& arg)
+void PrintAny (const boost::any& arg)
 {
+   using namespace TypeSwitch;
+
    Switch (
       arg,
-      Case<S1>([](const S1& a) {std::cout << "S1(" << a.value << ")"<< std::endl;}),
-      Case<S2>([](const S2& a) {std::cout << "S2(" << a.value << ")"<< std::endl;}),
-      Case<S2Derived>([](const S2Derived& a) {std::cout << "S2D(" << a.value << ")"<< std::endl;}),
-      //Case<S3Derived>([](const S3Derived& a) {std::cout << "S3D(" << a.value << ")"<< std::endl;}),
+      Case<S1>(
+         [](const S1& a)
+         {
+            std::cout << "S1(" << a.value << ")"<< std::endl;
+         }),
+      Case<S2>(
+         [](const S2& a)
+         {
+            std::cout << "S2(" << a.value << ")"<< std::endl;
+         }),
+      Case<S2Derived>(
+         [](const S2Derived& a)
+         {
+            std::cout << "S2D(" << a.value << ")"<< std::endl;
+         }),
       OneOf<S3, S4>(
          [](const boost::any& a)
          {
@@ -56,12 +69,70 @@ void Print (const boost::any& arg)
                IgnoreOthers ());
             std::cout << ")" << std::endl;
          }),
-      Otherwise ([](const boost::any& a) {std::cout << "Default" << std::endl;}));
+      Otherwise (
+         [](const boost::any& a)
+         {
+            std::cout << "Default" << std::endl;
+         })
+      );
+}
+
+void PrintS2 (const S2& arg)
+{
+   using namespace TypeSwitch;
+
+   Switch (
+      arg,
+      Case<S2>(
+         [](const S2& a)
+         {
+            std::cout << "S2(" << a.value << ")"<< std::endl;
+         }),
+      Case<S2Derived>(
+         [](const S2Derived& a)
+         {
+            std::cout << "S2D(" << a.value << ")"<< std::endl;
+         }),
+      Otherwise (
+         [](const S2& a)
+         {
+            std::cout << "Shouldn't get here!" << std::endl;
+         })
+      );
+}
+
+void PrintVariant (const boost::variant<S1, S2, S3>& v)
+{
+   using namespace TypeSwitch;
+
+   Switch (
+      v,
+      Case<S1>(
+         [](const S1& a)
+         {
+            std::cout << "S1(" << a.value << ")"<< std::endl;
+         }),
+      Case<S2>(
+         [](const S2& a)
+         {
+            std::cout << "S2(" << a.value << ")"<< std::endl;
+         }),
+      Case<S3>(
+         [](const S3& a)
+         {
+            std::cout << "S3(" << a.value << ")"<< std::endl;
+         }),
+      Otherwise (
+         [](const auto& a)
+         {
+            std::cout << "Shouldn't get here" << std::endl;
+         })
+      );
 }
 
 int main ()
 {
-   const auto s2d = boost::any (S2Derived {974496});
+   const auto s2d = boost::any {S2Derived {974496}};
    if (boost::any_cast<S2>(&s2d))
    {
       std::cout << "Got base ptr" << std::endl;
@@ -69,25 +140,43 @@ int main ()
    //boost::any_cast does not take inheritance into account!!!!
    
    std::cout << "S1 = ";
-   Print (S1 {299298});
+   PrintAny (S1 {299298});
 
    std::cout << "S2 = ";
-   Print (S2 {984707});
+   PrintAny (S2 {984707});
 
    std::cout << "S2Derived = ";
-   Print (S2Derived {779639});
+   PrintAny (S2Derived {779639});
 
    std::cout << "S3 = ";
-   Print (S3 ());
+   PrintAny (S3 {});
 
    std::cout << "S3Derived = ";
-   Print (S3Derived {376581});
+   PrintAny (S3Derived {376581});
 
    std::cout << "S4 = ";
-   Print (S4 ());
+   PrintAny (S4 {});
 
    std::cout << "S5 = ";
-   Print (S5 ());
+   PrintAny (S5 {});
+
+   std::cout << "S2: S2 = ";
+   PrintS2 (S2 {238263});
+   
+   std::cout << "S2: S2Derived = ";
+   PrintS2 (S2Derived {238263});
+
+   std::cout << "Variant: S1 = ";
+   PrintVariant (S1 {238263});
+
+   std::cout << "Variant: S2 = ";
+   PrintVariant (S2 {238263});
+
+   std::cout << "Variant: S3 = ";
+   PrintVariant (S3 {168066});
+   
+   std::cout << "Variant: S2Derived = ";
+   PrintVariant (S2Derived {238263});
 
    return 0;
 }
